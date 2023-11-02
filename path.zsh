a926cc61b2ade4e670c62e48dad9a15161ec45a0
Markus Alfter <git@alfter-web.de>
2023-11-02 15:43:42 +0100

    Update PATH

--- path.zsh
+++ path.zsh
@@ -1,1 +1,8 @@
-export PATH=~/.bin:$PATH
+# Add directory from Tool 'bin' to PATH (Manages binary files downloaded from different sources https://github.com/marcosnils/bin)
+export PATH=$HOME/bin:$PATH
+# Add .bin directory for local (custom) scripts to PATH
+export PATH=$HOME/.bin:$PATH
+# Add go to PATH
+export PATH=$PATH:/usr/local/go/bin
+# Add for python pipx
+export PATH=$HOME/.local/bin:$PATH
